e4d583d8105978afbfe9fdc590eba18933386f8b
davidcraig <david@davidcraig.eu>
2019-11-14 20:16:17 +0000

    Add more leather hides and scales

--- categories/profession/lw.lua
+++ categories/profession/lw.lua
@@ -4,6 +4,8 @@ AddonTable.lw = {
   ['name'] = 'P: Leatherworking',
   ['items'] = {
     -- Leather
+    [2934]  = true,  -- Ruined Leather Scraps
+    [2318]  = true,  -- Light Leather
     [2319]  = true,  -- Medium Leather
     [4234]  = true,  -- Heavy Leather
     [4304]  = true,  -- Thick Leather
@@ -11,12 +13,15 @@ AddonTable.lw = {
     [17012] = true, -- Core Leather
 
     -- Hides
+    [783]  =  true, -- Light Hide
+    [4232]  = true, -- Medium Hide
     [4235]  =  true, -- Heavy Hide
     [8169]  =  true, -- Thick Hide
     [8171]  =  true, -- Rugged Hide
     [15407] = true, -- Cured Rugged Hide
 
     -- Scales
+    [7286] = true, -- Black Whelp Scale
     [15408] = true, -- Heavy Scorpid Scale
 
     -- Other
