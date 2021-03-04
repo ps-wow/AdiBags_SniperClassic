ecee238b1fbf918ae0fd6516c496968d05dbe676
davidcraig <davidcraig1986@gmail.com>
2021-03-04 17:26:16 +0000

    Add runecloth and bolt of mageweave.

--- categories/profession/tailor.lua
+++ categories/profession/tailor.lua
@@ -9,9 +9,11 @@ AddonTable.tailor = {
     [2592] = true, -- Wool Cloth
     [4306] = true, -- Silk Cloth
     [4338] = true, -- Mageweave Cloth
+    [14047] = true, -- Runcloth
     -- Bolts
     [2997] = true, -- Bolt of Woolen Cloth
     [4305] = true, -- Bolt of Silk Cloth
+    [4339] = true, -- Bolt of Mageweave
     -- Other
     [3182] = true, -- Spider's Silk
   }
