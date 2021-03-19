dfe30b1a0c80c12cc041d371605d512bbecb28d4
davidcraig <davidcraig1986@gmail.com>
2021-03-19 16:46:28 +0000

    Defias Renegade Ring

--- categories/boe/armour/jewelry.lua
+++ categories/boe/armour/jewelry.lua
@@ -2,6 +2,7 @@ local AddonName, AddonTable = ...
 
 AddonTable.boe.jewelry = {
   -- Rings
+  [1076] = true, -- Defias Renegade Ring
   [11982] = true, -- Viridian Band
   [11994] = true, -- Coral Band
 }
