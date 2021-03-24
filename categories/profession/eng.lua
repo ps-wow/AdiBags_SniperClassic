0b33211fe24eb8763785c85767b44ecfef8d925a
davidcraig <davidcraig1986@gmail.com>
2021-03-24 21:48:48 +0000

    Add more engineering mats.

--- categories/profession/eng.lua
+++ categories/profession/eng.lua
@@ -3,8 +3,12 @@ local AddonName, AddonTable = ...
 AddonTable.eng = {
   ['name'] = 'P: Engineering',
   ['items'] = {
-    -- Ore/Bars
     [4361] = true, -- Copper Tube
     [4359] = true, -- Handful of Copper Bolts
+    [4363] = true, -- Copper Modulator
+    -- Powders
+    [4357] = true, -- Rough Blasting Powder
+    [4364] = true, -- Coarse Blasting Powder
+    [4377] = true, -- Heavy Blasting Powder
   }
 }
