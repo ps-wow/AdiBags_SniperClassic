4263881baf62c6fc0a79d6dfcdc69115f291fc14
davidcraig <davidcraig1986@gmail.com>
2021-05-03 19:35:19 +0100

    Add STV

---
+++ categories/quests/stv.lua
@@ -0,0 +1,23 @@
+local AddonName, AddonTable = ...
+
+
+AddonTable.stv = {
+  ['name'] = 'Q: Stranglethorn Vale',
+  ['items'] = {
+    -- Green Hills of Stranglethorn
+    --- Chapter 1
+    [2725] = true, -- Page 1
+    [2728] = true, -- Page 4
+    [2730] = true, -- Page 6
+    --- Chapter 2
+    [2734] = true, -- Page 10
+    [2735] = true, -- Page 11
+    [2740] = true, -- Page 16
+    --- Chapter 3
+    [2744] = true, -- Page 20
+    [2745] = true, -- Page 21
+    [2748] = true, -- Page 24
+    --- Chapter 4
+    [2749] = true, -- Page 25
+  }
+}
