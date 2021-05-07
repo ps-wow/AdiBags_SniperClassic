c3cece804e33c2f029fee5ba67240ededd23ab33
davidcraig <davidcraig1986@gmail.com>
2021-05-08 00:12:20 +0100

    Add small furry paw.

--- categories/misc/holidays/darkmoon.lua
+++ categories/misc/holidays/darkmoon.lua
@@ -3,9 +3,10 @@ local AddonName, AddonTable = ...
 AddonTable.darkmoon = {
   ['name'] = 'Hol: Darkmoon Faire',
   ['items'] = {
-    -- Grey Hand In's
+    -- DMF Hand In's
     [19933] = true, -- Glowing Scorpid Blood
     [4582] = true, -- Soft Bushy Tail
     [5117] = true, -- Vibrant Plume
+    [5134] = true, -- Small Furry Paw
   }
 }
